4803159ccbea2fb68703eb86419cb04ab70ff9d3
strugglebak <strugglebak@gmail.com>
2020-07-09 23:09:47 +0800

    feat: 两数相加

--- __test__/3-add-two-numbers.js
+++ __test__/3-add-two-numbers.js
@@ -1,58 +1,3 @@
-# leetcode-record
-
-leetcode 刷题记录
-
-## 两数之和
-
-最简单的解法
-
-```js
-var twoSum = function(nums, target) {
-  if (nums.length === 2) {
-      return [0, 1]
-  }
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
-      }
-    }
-  }
-}
-```
-
-## 无重复字符的最长子串
-
-```js
-var lengthOfLongestSubstring = function (s) {
-  let length = 0, str = ''
-  s.split('').forEach(item => {
-    // 两两比较大小
-    const index = str.indexOf(item)
-    if (index > -1) {
-      // 如果找到了，说明在这个字符串中有重复的字符
-      // 拼接字符串，将 abca -> bca
-      str = str.substring(index + 1, str.length) + item
-    } else {
-      // 如果没有找到，说明可以继续加字符
-      str += item
-    }
-
-    // 每次都比较两字符串的长度，只要最大的那个
-    length = Math.max(...[length, str.length])
-  })
-
-  return length
-}
-```
-
-虽然是抄的，但是这种解法最简单，在一次循环中通过两两比较出来的 length，最后得出来的总是最大的，很妙
-
-## 两数相加
-
-两数相加这个，看题目，原本思路是 数组 -> 字符串 -> 数字相加，但是后面看到也许数字很大以致于相加数字太大了所以采取 **链表** 的形式来做，瞬间明白了他的做法了。现在的基本思路就是先将对应的数组的数字加起来，然后后面再统一做一个进位的操作
-
-```js
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -139,6 +84,28 @@ function getNumArray(list) {
 
   return array
 }
-```
 
-的确不是很优雅的方式，待后面优化吧，返回过了哈哈哈
+// test code
+let l1 = new ListNode(6)
+l1 = insert(l1, 4)
+l1 = insert(l1, 5)
+l1 = insert(l1, 0)
+l1 = insert(l1, 4)
+l1 = insert(l1, 4)
+l1 = insert(l1, 9)
+l1 = insert(l1, 4)
+l1 = insert(l1, 1)
+console.log(l1)
+
+let l2 = new ListNode(3)
+l2 = insert(l2, 8)
+l2 = insert(l2, 8)
+l2 = insert(l2, 0)
+l2 = insert(l2, 3)
+l2 = insert(l2, 0)
+l2 = insert(l2, 1)
+l2 = insert(l2, 4)
+l2 = insert(l2, 8)
+console.log(l2)
+
+console.log(addTwoNumbers(l1, l2))
